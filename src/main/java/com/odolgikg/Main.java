e902cf0d6676a3ce496a83e064f5b541abc59166
Oleg Dolgikh <olegdolgikh@Olegs-MacBook-Pro.local>
2017-04-17 19:16:55 +0300

    first commit

---
+++ src/main/java/com/odolgikg/Main.java
@@ -0,0 +1,45 @@
+package com.odolgikg;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import okio.ByteString;
+
+/**
+ * Created by olegdolgikh on 4/13/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            CertificateFactory certFactory;
+            certFactory = CertificateFactory.getInstance("X.509");
+            InputStream inputStream = null;
+            try {
+                inputStream = new FileInputStream(args[0]);
+                X509Certificate cert = (X509Certificate) certFactory.generateCertificate(inputStream);
+                System.out.println(("[CERT] = " + ByteString.of(cert.getPublicKey().getEncoded()).sha1().base64()));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+    }
+}
